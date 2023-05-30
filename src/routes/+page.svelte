aac737177fc8c0ac4b83d5d9b9d28cd388072288
Peter Sysel <peter@sysel.at>
2023-05-30 03:45:37 +0200

    added cool moving flag with under construction text and moved the old demo page to the demo folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,93 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+    <title>Home</title>
+    <meta name="description" content="Under Construction"/>
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<div class="fullpage">
+    <div class="flag">
+        <div style="z-index: 14"  class="flagsection pink"></div>
+        <div style="z-index: 13"  class="flagsection blue"></div>
+        <div style="z-index: 12"  class="flagsection pink"></div>
+        <div style="z-index: 11"  class="flagsection white"><h1>Under Construction</h1></div>
+        <div style="z-index: 10"  class="flagsection pink"></div>
+        <div style="z-index: 09"  class="flagsection blue"></div>
+        <div style="z-index: 08"  class="flagsection pink"></div>
+        <div style="z-index: 07"  class="flagsection white"><h1>Under Construction</h1></div>
+        <div style="z-index: 06"  class="flagsection pink"></div>
+        <div style="z-index: 05"  class="flagsection blue"></div>
+        <div style="z-index: 04"  class="flagsection pink"></div>
+        <div style="z-index: 03"  class="flagsection white"><h1>Under Construction</h1></div>
+        <div style="z-index: 02"  class="flagsection pink"></div>
+        <div style="z-index: 01"  class="flagsection blue"></div>
+    </div>
+</div>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+    .fullpage {
+        height: 100vh;
+        width: 100vw;
+        position: fixed;
+    }
 
-	h1 {
-		width: 100%;
-	}
+    h1 {
+        z-index: 20;
+        font-family: "Courier New", monospace;
+        font-size: 4rem;
+    }
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+    .flagsection {
+        width: 100%;
+        height: 20vh;
+        box-shadow: 0 5px 10px 1px;
+        position: relative;
+        display: flex;
+        align-items: center;
+        padding: 0 2rem;
+    }
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    .flag {
+        position: relative;
+        display: flex;
+        height: 280vh;
+        flex-direction: column;
+        animation-name: scrolldown;
+        animation-duration: 4s;
+        animation-iteration-count: infinite;
+        animation-timing-function: cubic-bezier(.18,.89,.32,1.28);
+    }
+
+
+    .blue {
+        background-color: #5BCEFA;
+    }
+
+    .pink {
+        background-color: #F5A9B8;
+    }
+
+    .white {
+        background-color: #FFF;
+    }
+
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        overflow: hidden;
+    }
+
+    @keyframes scrolldown {
+        from {
+            transform: translateY(-20vh);
+        }
+        80%,
+        100% {
+            transform: translateY(-180vh);
+        }
+    }
 </style>
