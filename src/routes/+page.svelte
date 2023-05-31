3b4ee63cea0b0f3342f7c9e87f60ce69be8daabf
Peter Sysel <peter@sysel.at>
2023-06-01 01:05:46 +0200

    cool link and advanced shadow and bugfix animationstart

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,8 +12,8 @@
         <div style="z-index: 14" class="flagsection pink"></div>
         <div style="z-index: 13" class="flagsection blue"></div>
         <div style="z-index: 12" class="flagsection pink"></div>
-        <div style="z-index: 11" class="flagsection white"><h1>Under Construction</h1></div>
-        <div style="z-index: 10" class="flagsection pink"></div>
+        <div style="z-index: 11" class="flagsection white"><h1>Under Construction...</h1></div>
+        <div style="z-index: 10" class="flagsection pink"><a href="https://en.pronouns.page/@mari_ari"><h1 class="link">so have this instead (click)</h1></a></div>
         <div style="z-index: 09" class="flagsection blue"></div>
         <div style="z-index: 08" class="flagsection pink"></div>
         <div style="z-index: 07" class="flagsection white"><a href="https://soisi.mariari.me"><h1>Under
@@ -21,8 +21,8 @@
         <div style="z-index: 06" class="flagsection pink"></div>
         <div style="z-index: 05" class="flagsection blue"></div>
         <div style="z-index: 04" class="flagsection pink"></div>
-        <div style="z-index: 03" class="flagsection white"><h1>Under Construction</h1></div>
-        <div style="z-index: 02" class="flagsection pink"></div>
+        <div style="z-index: 03" class="flagsection white"><h1>Under Construction...</h1></div>
+        <div style="z-index: 02" class="flagsection pink"><a href="https://en.pronouns.page/@mari_ari"><h1 class="link">so have this instead (click)</h1></a></div>
         <div style="z-index: 01" class="flagsection blue"></div>
     </div>
 </div>
@@ -40,20 +40,31 @@
         font-family: "Courier New", monospace;
         font-size: 4rem;
         color: black;
+    }
 
+    a {
+        text-decoration: none;
+    }
 
+    .link {
+        text-decoration: underline dashed 5px #266032;
+        color: black;
+        font-size: 3rem;
     }
 
     @media only screen and (max-width: 750px) {
         h1 {
             font-size: 1.5rem;
         }
+        .link {
+            font-size: 1.0rem;
+        }
     }
 
     .flagsection {
         width: 100%;
         height: 20vh;
-        box-shadow: 0 5px 10px 1px;
+        box-shadow: 0 1px 10px 1px;
         position: relative;
         display: flex;
         align-items: center;
@@ -70,6 +81,7 @@
         animation-delay: 2s;
         animation-iteration-count: infinite;
         animation-timing-function: cubic-bezier(.77, 0, .33, .93);
+        transform: translateY(-20vh);
     }
 
 
